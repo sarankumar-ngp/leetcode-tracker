cd5a9d58c24711dabcb67731d6d727b58f69ddf6
sarankumar-ngp <24243096@drngpit.ac.in>
2026-07-28 15:44:16 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,29 @@
+// Last updated: 28/07/2026, 15:44:16
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
